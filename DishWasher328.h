443d3a5719923edee66eac560b887c5a8189c05d
viktor.kostylev <viktor.kostylev@mail.ru>
2022-07-03 02:54:08 +0300

    Added base substitutions

---
+++ DishWasher328.h
@@ -0,0 +1,13 @@
+#ifndef DISHWASHER328_H_
+#define DISHWASHER328_H_
+
+#include "Substitutions.h"
+
+#define F_CPU 6000000
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+
+#endif /* DISHWASHER328_H_ */
