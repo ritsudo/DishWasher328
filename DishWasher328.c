bcb44ac61b3a1a0c457a1b8a4fafd3ec2bf9a5f4
viktor.kostylev <viktor.kostylev@mail.ru>
2022-07-02 03:47:17 +0300

    Prj created for 328P

---
+++ DishWasher328.c
@@ -0,0 +1,17 @@
+/*
+ * DishWasher328.c
+ *
+ * Created: 02.07.2022 3:46:02
+ *  Author: Victor
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
